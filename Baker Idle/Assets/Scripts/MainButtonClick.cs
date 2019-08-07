6237be881221e3cc0f25ec15b8b8ac65006fe59a
Antic Kev <Antickev@gmail.com>
2019-08-08 00:27:06 +0100

    Game

---
+++ Baker Idle/Assets/Scripts/MainButtonClick.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainButtonClick : MonoBehaviour {
+
+    public GameObject textbox;
+
+    public void ClickTheButton ()
+    {
+        textbox.SetActive(true);
+    }
+	
+}
